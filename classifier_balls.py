b5b819a84b8eb20818c557dd4769bb9d0bdb03f7
osvaldo-murillo29 <osvaldo.murillo29@gmail.com>
2018-06-09 22:15:49 -0500

    Creation of the model for the CNN.

--- classifier_balls.py
+++ classifier_balls.py
@@ -11,3 +11,30 @@ model_weights_file = 'soccer_balls.h5'
 train_samples = 2000
 validation_samples = 800
 epoach = 50
+
+#Dimension of the Image
+img_width, img_height = 150, 150
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+
+train_samples = 2000
+validation_samples = 800
+
+epoch =50
+
+model = Sequential()
+
+model.add(Convolution2D(32,(3,3), input_shape=(img_width, img_height, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+(
+model.add(Convolution2D(32,(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
